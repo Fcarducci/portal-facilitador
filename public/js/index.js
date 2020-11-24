61d97b7d7ebd8edbdaba069671e0d29baabaffaf
Francesco Carducci <francesco.carducci@globalalumni.org>
2020-11-24 18:05:13 +0100

    navbar functionality ready

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,19 @@
 let anchoPantalla  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 console.log(anchoPantalla);
+
+
+let divColapsable = document.querySelector(".rrss-container");
+
+window.onload = function(){
+  if(anchoPantalla < 990 ){
+    divColapsable.classList.remove("rrss-container")
+  }
+}
+
+/*
+buttom.addEventListener("click", function(){
+  if(anchoPantalla < 990 ){
+
+  }
+})
+*/
