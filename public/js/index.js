fd6223e1aaf023ff16a09acf31aed2a2684e3c8e
Francesco Carducci <francesco.carducci@globalalumni.org>
2020-11-30 18:31:44 +0100

    added new menu

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,19 @@
 let anchoPantalla  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 console.log(anchoPantalla);
 
+let buttom = document.getElementById("boton-menu")
 
-let divColapsable = document.querySelector(".rrss-container");
-
-window.onload = function(){
-  if(anchoPantalla < 990 ){
-    divColapsable.classList.remove("rrss-container")
-  }
-}
+let divColapsable = document.querySelector(".menu-container");
 
 /*
-buttom.addEventListener("click", function(){
+window.onload = function(){
   if(anchoPantalla < 990 ){
-
+    divColapsable.classList.remove(".menu-container")
   }
-})
+}
 */
+
+buttom.addEventListener("click", function(){
+  console.log("HOLA")
+ divColapsable.classList.toggle("mt-4");
+});
